524f641b9de91de5b6de34ea31621e5771260b19
Tshepo <rocboyt@gmail.com>
2021-05-30 08:54:48 +0200

    Added launcher

---
+++ app/src/main/java/com/platform45/fx45/ui/convert/convertViewModel.kt
@@ -0,0 +1,4 @@
+package com.platform45.fx45.ui.convert
+
+class convertViewModel {
+}
---
+++ app/src/main/java/com/platform45/fx45/ui/history/MainViewModel.kt
@@ -0,0 +1,7 @@
+package com.platform45.fx45.ui.history
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
