b103efebd113262fe73de68a45add611dd9889a5
Tshepo <rocboyt@gmail.com>
2021-06-01 23:35:54 +0200

    Added convert destination

--- app/src/main/java/com/platform45/fx45/di/ViewModelModule.kt
+++ app/src/main/java/com/platform45/fx45/di/ViewModelModule.kt
@@ -1,5 +1,6 @@
 package com.platform45.fx45.di
 
+import com.platform45.fx45.ui.convert.ConversionViewModel
 import com.platform45.fx45.ui.dashboard.DashboardViewModel
 import org.koin.android.ext.koin.androidApplication
 import org.koin.androidx.viewmodel.dsl.viewModel
@@ -7,5 +8,5 @@ import org.koin.dsl.module
 
 val viewModelModule = module {
     viewModel { DashboardViewModel(androidApplication(), get()) }
-    //viewModel { ConversionViewModel(androidApplication(), get()) }
+    viewModel { ConversionViewModel(androidApplication(), get()) }
 }
