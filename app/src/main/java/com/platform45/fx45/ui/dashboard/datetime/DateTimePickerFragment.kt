49c973fcd383609bce7a750a0a7428448861a890
Tshepo <rocboyt@gmail.com>
2021-06-22 12:07:16 +0200

    refactored date picker

--- app/src/main/java/com/platform45/fx45/ui/dashboard/datetime/DateTimePickerFragment.kt
+++ app/src/main/java/com/platform45/fx45/ui/dashboard/datetime/DateTimePickerFragment.kt
@@ -10,32 +10,21 @@ import android.widget.TimePicker
 import com.platform45.fx45.R
 import com.platform45.fx45.base.fragments.BaseDialogFragment
 import com.platform45.fx45.constants.DATETIME
+import kotlinx.android.synthetic.main.fragment_date_time_picker.*
 import java.sql.Time
 import java.text.Format
 import java.text.SimpleDateFormat
 
 class DateTimePickerFragment : BaseDialogFragment(){
     private var dateTimeContext: DateTimeSetter? = null
-    private var datePickerContainerLl: View? = null
-    private var timePickerContainerLl: View? = null
-    private var selectedDateDp: DatePicker? = null
-    private var selectedTimeTp: TimePicker? = null
-    protected var dateTimeNextBtn: Button? = null
-    protected var dateTimeBackBtn: Button? = null
-    protected var dateTimeDoneBtn: Button? = null
-
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         dateTimeContext = parentFragment as DateTimeSetter
     }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val parentView = super.onCreateView(inflater, container, savedInstanceState)
-        initViews(parentView)
-        return parentView
-    }
-
-    fun initViews(parentView: View?) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
         val dateTime = arguments?.getString(DATETIME)
         val ymd = dateTime?.split("-")
         val y = ymd?.get(0)?.toInt() ?: 0
@@ -43,25 +32,17 @@ class DateTimePickerFragment : BaseDialogFragment(){
         m =  if(m > 0) m  - 1 else  m
         val d = ymd?.get(2)?.toInt()  ?: 0
 
-        datePickerContainerLl = parentView?.findViewById(R.id.llDatePickerContainer)
-        selectedDateDp = parentView?.findViewById(R.id.dpAppointmentDate)
-        selectedDateDp?.init(y, m, d, null)
+        dpAppointmentDate?.init(y, m, d, null)
 
-        dateTimeNextBtn = parentView?.findViewById(R.id.btnDateTimeNext)
-        dateTimeNextBtn?.setOnClickListener {
+        btnDateTimeNext?.setOnClickListener {
             showTimePicker()
         }
 
-        timePickerContainerLl = parentView?.findViewById(R.id.llTimePickerContainer)
-        selectedTimeTp = parentView?.findViewById(R.id.tpAppointmentTime)
-
-        dateTimeBackBtn = parentView?.findViewById(R.id.btnDateTimeBack)
-        dateTimeBackBtn?.setOnClickListener{
+        btnDateTimeBack?.setOnClickListener{
             showDatePicker()
         }
 
-        dateTimeDoneBtn = parentView?.findViewById(R.id.btnDateTimeDone)
-        dateTimeDoneBtn?.setOnClickListener {
+        btnDateTimeDone?.setOnClickListener {
             setTdateTime()
             dismiss()
         }
@@ -70,37 +51,36 @@ class DateTimePickerFragment : BaseDialogFragment(){
     }
 
     fun showDatePicker(){
-        datePickerContainerLl?.visibility = View.VISIBLE
-        timePickerContainerLl?.visibility = View.GONE
-        dateTimeBackBtn?.visibility = View.GONE
-        dateTimeNextBtn?.visibility = View.VISIBLE
-        dateTimeDoneBtn?.visibility = View.VISIBLE
+        llDatePickerContainer?.visibility = View.VISIBLE
+        llTimePickerContainer?.visibility = View.GONE
+        btnDateTimeBack?.visibility = View.GONE
+        btnDateTimeNext?.visibility = View.VISIBLE
+        btnDateTimeDone?.visibility = View.VISIBLE
     }
 
     fun showTimePicker(){
-        datePickerContainerLl?.visibility = View.GONE
-        timePickerContainerLl?.visibility = View.VISIBLE
-        dateTimeBackBtn?.visibility = View.VISIBLE
-        dateTimeNextBtn?.visibility = View.GONE
-        dateTimeDoneBtn?.visibility = View.VISIBLE
+        llDatePickerContainer?.visibility = View.GONE
+        llTimePickerContainer?.visibility = View.VISIBLE
+        btnDateTimeBack?.visibility = View.VISIBLE
+        btnDateTimeNext?.visibility = View.GONE
+        btnDateTimeDone?.visibility = View.VISIBLE
     }
 
     private fun getTime(hr: Int, min: Int): String {
-        val tme = Time(hr, min, 0)//seconds by default set to zero
+        val tme = Time(hr, min, 0)
         val formatter: Format
-        //formatter = SimpleDateFormat("h:mm a")
         formatter = SimpleDateFormat("h:mm")
         return formatter.format(tme)
     }
 
     fun setTdateTime() {
-        val year = selectedDateDp!!.year
-        val month = if(selectedDateDp!!.month > 0) selectedDateDp!!.month + 1 else 0
-        val day = selectedDateDp!!.dayOfMonth
+        val year = dpAppointmentDate!!.year
+        val month = if(dpAppointmentDate!!.month > 0) dpAppointmentDate!!.month + 1 else 0
+        val day = dpAppointmentDate!!.dayOfMonth
         dateTimeContext?.setDate(year, month, day)
 
-        val hour = selectedTimeTp?.currentHour ?: 0
-        val min = selectedTimeTp?.currentMinute ?: 0
+        val hour = tpAppointmentTime?.currentHour ?: 0
+        val min = tpAppointmentTime?.currentMinute ?: 0
         val time = getTime(hour, min)
         dateTimeContext?.setTime(time)
     }
