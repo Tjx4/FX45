4385ede46d05b7e61f199452b9d11ba676e4a01b
Tshepo <rocboyt@gmail.com>
2021-12-01 21:27:28 +0200

    reset

---
+++ app/src/main/java/com/platform45/fx45/networking/retrofit/Hosts.kt
@@ -0,0 +1,5 @@
+package com.platform45.fx45.networking.retrofit
+
+enum class Hosts(var url: String) {
+    Production("https://fxmarketapi.com/"),
+}
---
+++ app/src/main/java/com/platform45/fx45/networking/retrofit/RetrofitHelper.kt
@@ -0,0 +1,18 @@
+package com.platform45.fx45.networking.retrofit
+
+
+import retrofit2.http.*
+import xyz.appic.common.models.Conversion
+import xyz.appic.common.models.Currencies
+import xyz.appic.common.models.Series
+
+interface RetrofitHelper {
+    @GET("apiconvert")
+    suspend fun convert(@Query("api_key") apiKey: String, @Query("from") from: String, @Query("to") to: String, @Query("amount")  amount: String): Conversion?
+
+    @GET("apitimeseries")
+    suspend fun series(@Query("api_key") apiKey: String, @Query("start_date") startDate: String, @Query("end_date") endDate: String, @Query("currency") currency: String, @Query("format") format: String): Series?
+
+    @GET("apicurrencies")
+    suspend fun currencies(@Query("api_key") apiKey: String): Currencies?
+}
